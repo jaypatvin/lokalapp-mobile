e2957cdb4b0a2a607fa554cde37d7638fe0413e0
Jay Jamero <jay@jamero.me>
2020-11-27 23:18:29 +0800

    modified invite page screen and components

--- lib/widgets/rounded_button.dart
+++ lib/widgets/rounded_button.dart
@@ -1,29 +1,28 @@
-
+import 'package:flutter/material.dart';
 import 'package:lokalapp/utils/themes.dart';
 
 class RoundedButton extends StatelessWidget {
   final String label;
   final Function onPressed;
-  final double minWidth;
 
-  RoundedButton({this.label, this.onPressed, this.minWidth = 113.0});
+  RoundedButton({this.label, this.onPressed});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16.0),
-      child: Material(
-        color: kTealColor,
+    return FlatButton(
+      onPressed: onPressed,
+      color: kTealColor,
+      shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(30.0),
-        child: MaterialButton(
-          onPressed: this.onPressed,
-          minWidth: this.minWidth,
-          child: Text(
-            this.label,
-            style: TextStyle(
-              color: kNavyColor,
-            ),
-          ),
+      ),
+      minWidth: MediaQuery.of(context).size.width / 3,
+      child: Text(
+        this.label,
+        style: TextStyle(
+          color: kNavyColor,
+          fontFamily: "Goldplay",
+          fontWeight: FontWeight.w800,
+          fontSize: 14.0,
         ),
       ),
     );
