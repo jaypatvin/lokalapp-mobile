108a75f9895edae9b8a96f131501ff4cfbc80153
kd ang <kdang82590@gmail.com>
2020-11-26 19:57:49 -0600

    added welcome screen and splash screen

---
+++ lib/screens/invite_code.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InviteCode extends StatefulWidget {
+  @override
+  _InviteCodeState createState() => _InviteCodeState();
+}
+
+class _InviteCodeState extends State<InviteCode> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+    );
+  }
+}
