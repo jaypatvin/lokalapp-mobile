af6c785f518a7e271847750cd7e12a3a3ec2d2c5
Jay Jamero <jay@jamero.me>
2021-02-05 01:25:03 +0800

    update invite code to use API endpoint

---
+++ lib/services/lokal_api_service.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class LokalApiService {
+  static const _baseUrl =
+      'https://us-central1-lokal-1baac.cloudfunctions.net/api/v1';
+
+  Future<http.Response> createUser(Map data) async {
+    var body = json.encode(data);
+    return await http.post(
+      "$_baseUrl/users",
+      headers: {"Content-Type": "application/json"},
+      body: body,
+    );
+  }
+
+  Future<http.Response> checkInviteCode(String inviteCode) async {
+    return await http.get("$_baseUrl/invite/check/$inviteCode");
+  }
+
+  Future<http.Response> claimInviteCode(
+      String userUid, String inviteCode) async {
+    String body = json.encode(
+      {
+        "user_id": userUid,
+        "code": inviteCode,
+      },
+    );
+    return await http.post(
+      "$_baseUrl/invite/claim",
+      headers: {"Content-Type": "application/json"},
+      body: body,
+    );
+  }
+}
