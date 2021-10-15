4f0028d81835d45eeabea75db7e3718806a67c5f
Jay Jamero <jay@jamero.me>
2021-10-15 13:49:02 +0800

    remove unused dependencies

--- lib/screens/profile_screens/components/store_rating.dart
+++ lib/screens/profile_screens/components/store_rating.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_simple_rating_bar/flutter_simple_rating_bar.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class StoreRating extends StatelessWidget {
   @override
@@ -10,34 +10,50 @@ class StoreRating extends StatelessWidget {
         Container(
           alignment: Alignment.centerRight,
           padding: EdgeInsets.all(10),
-          child: RatingBar(
-            rating: 4,
-            icon: Icon(
-              Icons.star,
-              size: 20,
-              color: Colors.grey.shade300,
-            ),
-            starCount: 5,
-            spacing: 12.0,
-            size: 1,
-            isIndicator: true,
+          // child: RatingBar(
+          //   rating: 4,
+          //   icon: Icon(
+          //     Icons.star,
+          //     size: 20,
+          //     color: Colors.grey.shade300,
+          //   ),
+          //   starCount: 5,
+          //   spacing: 12.0,
+          //   size: 1,
+          //   isIndicator: true,
+          //   allowHalfRating: true,
+          //   onRatingCallback: (double value, ValueNotifier<bool> isIndicator) {
+          //     print('Number of stars-->  $value');
+          //     //change the isIndicator from false  to true ,the RatingBar cannot support touch event;
+          //     isIndicator.value = true;
+          //   },
+          //   clickedCallbackAsIndicator: () {
+          //     // when isIndicator is true ,user click the stars, this callback can be called.
+          //     print('clickedMe');
+          //   },
+          //   color: Colors.amber,
+          // ),
+          child: RatingBar.builder(
+            initialRating: 4.5,
+            minRating: 1,
+            maxRating: 5,
             allowHalfRating: true,
-            onRatingCallback: (double value, ValueNotifier<bool> isIndicator) {
-              print('Number of stars-->  $value');
-              //change the isIndicator from false  to true ,the RatingBar cannot support touch event;
-              isIndicator.value = true;
+            unratedColor: Colors.grey.shade300,
+            itemBuilder: (ctx, _) {
+              return Icon(
+                Icons.star,
+                color: Colors.amber,
+              );
             },
-            clickedCallbackAsIndicator: () {
-              // when isIndicator is true ,user click the stars, this callback can be called.
-              print('clickedMe');
+            onRatingUpdate: (rating) {
+              debugPrint(rating.toString());
             },
-            color: Colors.amber,
           ),
         ),
         Container(
           padding: const EdgeInsets.only(top: 20),
           child: Text(
-            "4.45",
+            "4.5",
             textAlign: TextAlign.center,
             style: TextStyle(color: Colors.amber),
           ),
