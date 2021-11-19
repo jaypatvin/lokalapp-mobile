f3496d102b4cdd4cdb261d278d47157fcc99463a
Jay Jamero <jay@jamero.me>
2021-11-19 18:27:25 +0800

    change flutter_html to flutter_webview

--- lib/screens/profile/settings/privacy_policy.dart/privacy_policy.dart
+++ lib/screens/profile/settings/privacy_policy.dart/privacy_policy.dart
@@ -1,45 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_html/flutter_html.dart';
-import 'package:lottie/lottie.dart';
-import 'package:provider/provider.dart';
 
 import '../../../../utils/constants/assets.dart';
-import '../../../../utils/constants/themes.dart';
-import '../../../../view_models/profile/settings/privacy_policy.vm.dart';
-import '../../../../widgets/custom_app_bar.dart';
+import '../../../../widgets/webview_builder.dart';
 
 class PrivacyPolicy extends StatelessWidget {
+  const PrivacyPolicy({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CustomAppBar(
-        backgroundColor: kTealColor,
-        titleText: 'Privacy Policy',
-        titleStyle: TextStyle(color: Colors.white),
-        onPressedLeading: () => Navigator.pop(context),
-      ),
-      body: ChangeNotifierProvider<PrivacyPolicyViewModel>(
-        create: (ctx) => PrivacyPolicyViewModel(ctx)..init(),
-        builder: (ctx, _) {
-          return Consumer<PrivacyPolicyViewModel>(
-            builder: (ctx2, vm, _) {
-              return SingleChildScrollView(
-                child: vm.isLoading
-                    ? Center(
-                        child: Lottie.asset(
-                          kAnimationLoading,
-                          fit: BoxFit.cover,
-                        ),
-                      )
-                    :  Html(
-                            data: vm.html,
-                          ),
-              );
-            },
-          );
-        },
-      ),
+    return WebViewPage(
+      url: kPrivacyPolicyUrl,
+      title: 'Privacy Policy',
     );
   }
 }
