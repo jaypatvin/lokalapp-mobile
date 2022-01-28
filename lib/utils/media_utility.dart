e388d6c4f385fbddcd047349f564a344f1b60a1f
jaypatvin <jaypatvin95@gmail.com>
2022-01-28 18:16:16 +0800

    add cupertinoAlertDialog to ImagePicker

--- lib/utils/media_utility.dart
+++ lib/utils/media_utility.dart
@@ -1,28 +1,53 @@
 import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 class MediaUtility {
-  static MediaUtility? _utility;
-  final _picker = ImagePicker();
-
-  static MediaUtility? get instance {
-    return _utility ??= MediaUtility();
-  }
+  final ImagePicker imagePicker = ImagePicker();
 
   Future<File?> _pickFile(ImageSource source) async {
-    final pickedImage = await _picker.pickImage(source: source);
+    final pickedImage = await imagePicker.pickImage(source: source);
     if (pickedImage != null) {
       return File(pickedImage.path);
     }
     return null;
   }
 
+  /// Shows the dialog to the user. [BuildContext] is required for the
+  /// engine to determine where to draw the dialog.
   Future<File?> showMediaDialog(BuildContext parentContext) async {
     return showDialog<File>(
       context: parentContext,
       builder: (context) {
+        if (Platform.isIOS) {
+          return CupertinoAlertDialog(
+            title: const Text('Upload Picture'),
+            content: const Text('Choose image source:'),
+            actions: [
+              CupertinoDialogAction(
+                child: const Text('Camera'),
+                onPressed: () async {
+                  final file = await _pickFile(ImageSource.camera);
+                  Navigator.pop(context, file);
+                },
+              ),
+              CupertinoDialogAction(
+                child: const Text('Gallery'),
+                onPressed: () async {
+                  final file = await _pickFile(ImageSource.gallery);
+                  Navigator.pop(context, file);
+                },
+              ),
+              CupertinoDialogAction(
+                child: const Text('Cancel'),
+                onPressed: () => Navigator.pop(context),
+              ),
+            ],
+          );
+        }
+
         return SimpleDialog(
           title: const Text('Upload Picture'),
           children: [
