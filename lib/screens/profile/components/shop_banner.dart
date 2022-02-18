77e47340d588ca4917687c8b77e6eb717443d5bc
jaypatvin <jaypatvin95@gmail.com>
2022-02-18 18:13:10 +0800

    fix user shop banner for registration updates

--- lib/screens/profile/components/shop_banner.dart
+++ lib/screens/profile/components/shop_banner.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 
+import '../../../models/shop.dart';
 import '../../../providers/auth.dart';
 import '../../../providers/shops.dart';
 import '../../../state/mvvm_builder.widget.dart';
@@ -31,156 +31,167 @@ class ShopBanner extends StatelessWidget {
 class _ShopBannerView extends HookView<ShopBannerViewModel> {
   @override
   Widget render(BuildContext context, ShopBannerViewModel vm) {
-    final _auth = useMemoized<Auth>(() => context.read<Auth>(), []);
-    final _shops = useMemoized<Shops>(() => context.read<Shops>(), []);
-
-    useEffect(
-      () {
-        void _listener() {
-          vm.refresh();
-        }
-
-        _auth.addListener(_listener);
-        _shops.addListener(_listener);
-
-        return () {
-          _auth.removeListener(_listener);
-          _shops.removeListener(_listener);
-        };
-      },
-      [vm],
-    );
-
-    final _buildShopBanner = useCallback<StatelessWidget Function()>(
-      () {
-        if (!vm.isUserRegistered &&
-            (vm.user.registration?.idPhoto?.isNotEmpty ?? false)) {
-          return GestureDetector(
-            onTap: vm.onVerify,
-            child: Container(
-              color: Colors.white,
-              width: double.infinity,
-              padding: EdgeInsets.symmetric(vertical: 16.0.h),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    'Verification Pending',
-                    style: Theme.of(context)
-                        .textTheme
-                        .headline6
-                        ?.copyWith(color: Colors.black),
-                  ),
-                  Text(
-                    'We are currently verifying your account.',
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodyText2
-                        ?.copyWith(color: Colors.grey),
-                  ),
-                ],
-              ),
-            ),
-          );
-        } else if (!vm.isUserRegistered) {
-          return ListView(
-            shrinkWrap: true,
-            children: [
-              ListTile(
-                tileColor: kPinkColor,
-                title: const Text(
-                  'Verify Account',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                trailing: Icon(
-                  Icons.arrow_forward_ios,
-                  color: Colors.white,
-                  size: 14.0.r,
-                ),
-                onTap: vm.onVerify,
-              ),
-              SizedBox(height: 10.h),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 75.0.w),
-                color: Colors.transparent,
-                child: const Text(
-                  'You must verify your account to add a shop',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: kPinkColor,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-              ),
-              SizedBox(height: 5.0.h),
-            ],
-          );
-        }
-
-        return Consumer<Shops>(
-          builder: (_, shops, __) {
-            if (shops.isLoading) {
-              return const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8.0),
-                child: Center(
-                  child: LinearProgressIndicator(),
-                ),
-              );
-            }
-
-            if (vm.shop == null) {
-              return Container(
-                color: Colors.white,
-                width: double.infinity,
-                padding: EdgeInsets.symmetric(
-                  vertical: 10.0.h,
-                  horizontal: 5.0.w,
-                ),
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 100.0.w),
-                  child: AppButton.transparent(
-                    text: '+ ADD SHOP',
-                    onPressed: vm.onAddShop,
-                  ),
-                ),
-              );
-            }
+    if (vm.isCurrentUser) {
+      return _CurrentUserShopBanner(
+        onVerifyPressed: vm.onVerify,
+        onAddShop: vm.onAddShop,
+        onGoToShop: vm.goToShop,
+      );
+    } else {
+      return Consumer<Shops>(
+        builder: (context, shops, _) {
+          final _shops = shops.findByUser(vm.userId);
+          if (_shops.isEmpty) {
+            return const SizedBox.shrink();
+          } else {
+            final _shop = _shops.first;
             return ShopTile(
-              shop: vm.shop!,
-              onGoToShop: vm.goToShop,
+              shop: _shop,
+              onGoToShop: () => vm.goToShop(_shop),
+            );
+          }
+        },
+      );
+    }
+  }
+}
+
+class _CurrentUserShopBanner extends StatelessWidget {
+  const _CurrentUserShopBanner({
+    Key? key,
+    required this.onVerifyPressed,
+    required this.onAddShop,
+    required this.onGoToShop,
+  }) : super(key: key);
+  final void Function() onVerifyPressed;
+  final void Function() onAddShop;
+  final void Function(Shop shop) onGoToShop;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: EdgeInsets.fromLTRB(10.0.w, 20.0.w, 10.0.w, 10.0.w),
+          width: double.infinity,
+          child: const Text(
+            'My Shop',
+            style: TextStyle(
+              color: kTealColor,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+        Consumer<Auth>(
+          builder: (context, auth, child) {
+            final _isUserRegistered =
+                auth.user?.registration?.verified ?? false;
+            if (!_isUserRegistered &&
+                (auth.user?.registration?.idPhoto?.isNotEmpty ?? false)) {
+              return GestureDetector(
+                onTap: onVerifyPressed,
+                child: Container(
+                  color: Colors.white,
+                  width: double.infinity,
+                  padding: EdgeInsets.symmetric(vertical: 16.0.h),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        'Verification Pending',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline6
+                            ?.copyWith(color: Colors.black),
+                      ),
+                      Text(
+                        'We are currently verifying your account.',
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyText2
+                            ?.copyWith(color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            } else if (!_isUserRegistered) {
+              return ListView(
+                shrinkWrap: true,
+                children: [
+                  ListTile(
+                    tileColor: kPinkColor,
+                    title: const Text(
+                      'Verify Account',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    trailing: Icon(
+                      Icons.arrow_forward_ios,
+                      color: Colors.white,
+                      size: 14.0.r,
+                    ),
+                    onTap: onVerifyPressed,
+                  ),
+                  SizedBox(height: 10.h),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 75.0.w),
+                    color: Colors.transparent,
+                    child: const Text(
+                      'You must verify your account to add a shop',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: kPinkColor,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 5.0.h),
+                ],
+              );
+            }
+
+            return Consumer<Shops>(
+              builder: (_, shops, __) {
+                if (shops.isLoading) {
+                  return const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Center(
+                      child: LinearProgressIndicator(),
+                    ),
+                  );
+                }
+
+                final _shops = shops.findByUser(auth.user!.id);
+                if (_shops.isEmpty) {
+                  return Container(
+                    color: Colors.white,
+                    width: double.infinity,
+                    padding: EdgeInsets.symmetric(
+                      vertical: 10.0.h,
+                      horizontal: 5.0.w,
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 100.0.w),
+                      child: AppButton.transparent(
+                        text: '+ ADD SHOP',
+                        onPressed: onAddShop,
+                      ),
+                    ),
+                  );
+                }
+                return ShopTile(
+                  shop: _shops.first,
+                  onGoToShop: () => onGoToShop(_shops.first),
+                );
+              },
             );
           },
-        );
-      },
-      [vm],
+        ),
+      ],
     );
-
-    switch (vm.mode) {
-      case ShopBannerMode.otherUserNoShop:
-        return const SizedBox();
-      case ShopBannerMode.otherUserWithShop:
-        return ShopTile(shop: vm.shop!, onGoToShop: vm.goToShop);
-      case ShopBannerMode.currentUser:
-        return Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              padding: EdgeInsets.fromLTRB(10.0.w, 20.0.w, 10.0.w, 10.0.w),
-              width: double.infinity,
-              child: const Text(
-                'My Shop',
-                style: TextStyle(
-                  color: kTealColor,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
-            _buildShopBanner(),
-          ],
-        );
-    }
   }
 }
--- lib/view_models/profile/components/shop_banner.vm.dart
+++ lib/view_models/profile/components/shop_banner.vm.dart
@@ -1,11 +1,8 @@
 import 'package:provider/provider.dart';
 
 import '../../../models/app_navigator.dart';
-import '../../../models/lokal_user.dart';
 import '../../../models/shop.dart';
 import '../../../providers/auth.dart';
-import '../../../providers/shops.dart';
-import '../../../providers/users.dart';
 import '../../../routers/app_router.dart';
 import '../../../routers/profile/props/user_shop.props.dart';
 import '../../../screens/profile/add_shop/add_shop.dart';
@@ -13,52 +10,16 @@ import '../../../screens/profile/shop/user_shop.dart';
 import '../../../state/view_model.dart';
 import '../../../widgets/verification/verify_screen.dart';
 
-enum ShopBannerMode {
-  currentUser,
-  otherUserNoShop,
-  otherUserWithShop,
-}
-
 class ShopBannerViewModel extends ViewModel {
   ShopBannerViewModel({required this.userId});
   final String userId;
 
   late final bool isCurrentUser;
-  late LokalUser _user;
-  LokalUser get user => _user;
 
-  Shop? shop;
-  ShopBannerMode mode = ShopBannerMode.currentUser;
-  bool get isUserRegistered => _user.registration?.verified ?? false;
 
   @override
   void init() {
     isCurrentUser = context.read<Auth>().user!.id == userId;
-    if (isCurrentUser) {
-      _userSetup();
-    } else {
-      _user = context.read<Users>().findById(userId)!;
-    }
-    _shopSetup();
-  }
-
-  void _shopSetup() {
-    final shops = context.read<Shops>().findByUser(userId);
-    if (shops.isNotEmpty) shop = shops.first;
-
-    if (!isCurrentUser) {
-      if (shops.isEmpty) {
-        mode = ShopBannerMode.otherUserNoShop;
-      } else {
-        mode = ShopBannerMode.otherUserWithShop;
-      }
-    } else {
-      mode = ShopBannerMode.currentUser;
-    }
-  }
-
-  void _userSetup() {
-    _user = context.read<Auth>().user!;
   }
 
   void onAddShop() {
@@ -81,12 +42,12 @@ class ShopBannerViewModel extends ViewModel {
     );
   }
 
-  Future<dynamic> goToShop() async {
-    if (userId == context.read<Auth>().user?.id) {
+  Future<dynamic> goToShop(Shop shop) async {
+    if (isCurrentUser) {
       return context.read<AppRouter>().navigateTo(
             AppRoute.profile,
             UserShop.routeName,
-            arguments: UserShopProps(userId, shop?.id),
+            arguments: UserShopProps(userId, shop.id),
           );
     }
 
@@ -96,15 +57,9 @@ class ShopBannerViewModel extends ViewModel {
           AppNavigator.appPageRoute(
             builder: (_) => UserShop(
               userId: userId,
-              shopId: shop?.id,
+              shopId: shop.id,
             ),
           ),
         );
-  }
-
-  void refresh() {
-    _shopSetup();
-    _userSetup();
-    notifyListeners();
   }
 }
